becc0f436108fb22d0893d078141322d13939f9b
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-06-26 15:02:20 +0200

    Esercizio 3 completato

--- src/main/java/maurotuzzolino/Main.java
+++ src/main/java/maurotuzzolino/Main.java
@@ -1,6 +1,7 @@
 package maurotuzzolino;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -16,6 +17,8 @@ public class Main {
         Product product2 = new Product(2L, "Libro", "Cultura", 20.0);
         Product product3 = new Product(3L, "Smartphone", "Elettronica", 800.0);
         Product product4 = new Product(4L, "Zaino", "Accessori", 50.0);
+        Product product5 = new Product(5L, "TV", "Elettronica", 1300.0);
+        Product product6 = new Product(6L, "Auricolari", "Elettronica", 150.0);
 
         Order order1 = new Order(1L, "Spedito", LocalDate.now().minusDays(5), LocalDate.now(), List.of(product1, product2), customer1);
         Order order2 = new Order(2L, "In elaborazione", LocalDate.now().minusDays(2), LocalDate.now().plusDays(3), List.of(product3), customer2);
@@ -24,6 +27,8 @@ public class Main {
         Order order5 = new Order(5L, "In elaborazione", LocalDate.now(), LocalDate.now().plusDays(4), List.of(product1), customer2);
 
         List<Order> allOrders = List.of(order1, order2, order3, order4, order5);
+        List<Product> products = List.of(product1, product2, product3, product4, product5, product6);
+
 
         System.out.println("========== Parte 1 ==========");
         Map<String, List<Order>> ordersByCustomer = allOrders.stream()
@@ -50,5 +55,14 @@ public class Main {
             System.out.println("Cliente: " + customer.getName() + " | Totale acquisti: €" + total);
         });
 
+
+        System.out.println("\n========== Parte 3 ==========");
+        List<Product> top3ExpensiveProducts = products.stream()
+                .sorted(Comparator.comparingDouble(Product::getPrice).reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+        
+        top3ExpensiveProducts.forEach(p ->
+                System.out.println(p.getName() + " - €" + p.getPrice()));
     }
 }
